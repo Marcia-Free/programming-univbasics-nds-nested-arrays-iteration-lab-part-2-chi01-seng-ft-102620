5b95e03f5872f125789e4635a117f557dfc35ccc
Marcia Ann Freeman <marciafreeman2011@gmail.com>
2020-10-11 23:12:43 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -8,7 +8,7 @@ row_index = 0
   while row_index < src.length do
     element_index = 0
     
-    min_values << src[element_index].min()
+    min_values << src[row_index].min()
     
     
       #while element_index < src[row_index].length do
